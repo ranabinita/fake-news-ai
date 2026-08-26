cf4f9a031979400e18618e0e38c0cb2b7e70b1ae
Raman Singh <ramansinghchauhan787@gmail.com>
2026-08-26 14:53:32 +0545

    setup React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Raman</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
